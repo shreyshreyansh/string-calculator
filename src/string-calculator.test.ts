2c64d2a570781ff7b40acd5d8aa6eecaa8aa634d
Shreyansh shrey <shreyansh.shrey1999@gmail.com>
2024-07-03 13:40:54 +0530

    Support Different Delimiters

--- src/string-calculator.test.ts
+++ src/string-calculator.test.ts
@@ -1,5 +1,5 @@
 import { add } from './string-calculator';
 
-test('should handle new lines between numbers', () => {
-  expect(add('1\n2,3')).toBe(6);
+test('should support different delimiters', () => {
+  expect(add('//;\n1;2')).toBe(3);
 });
--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -1,5 +1,13 @@
 export function add(numbers: string): number {
   if (numbers === '') return 0;
-  const numArray = numbers.split(/[\n,]/).map((num) => parseInt(num));
+
+  let delimiter = /[\n,]/;
+  if (numbers.startsWith('//')) {
+    const parts = numbers.split('\n');
+    delimiter = new RegExp(parts[0].slice(2));
+    numbers = parts[1];
+  }
+
+  const numArray = numbers.split(delimiter).map((num) => parseInt(num));
   return numArray.reduce((acc, curr) => acc + curr, 0);
 }
